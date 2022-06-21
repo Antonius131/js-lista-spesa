f33282919b59764b25bb9cf27c2b76793725043b
Antonius131 <antomendetta@gmail.com>
2022-06-21 15:13:12 +0200

    put the array in the DOM

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ const buyList = [
    'insalata',
    'avocado'
 ]
+
+const listWrapper = document.getElementById('buying-list');
+
+
+let i = 0;
+
+while ( i < buyList.length) {
+
+   const listTag = document.createElement('li');
+   listTag.innerHTML = buyList[i];
+
+   listWrapper.append(listTag);
+
+   i++;
+
+}
